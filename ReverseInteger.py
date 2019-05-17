910c2a4ae15049c2bc891b7566a956f3a1650c34
gokular <50200791+gokular@users.noreply.github.com>
2019-05-16 20:03:48 -0700

    Reverses the order of any 32 bit integer

---
+++ ReverseInteger.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(x: int) -> int:
+        if(x < -2147483648 or x > 2147483647):
+            return 0
+        stringNumber = str(x)
+        if(stringNumber[0] != '-'):
+            numberArray = str(x)[::-1]
+            result = int(numberArray)
+            if(result < -2147483648 or result > 2147483647):
+                return 0
+            return result
+        else:
+            stringNumber = stringNumber[1:len(str(x))]
+            numberArray = stringNumber[::-1]
+            result = int('-' + numberArray)
+            if(result < -2147483648 or result > 2147483647):
+                return 0
+            return result
+    
+    print(reverse(123))
